89ea40ed0ec746e27128bf81ca20992412a4684a
bmackin <mackin.beth@gmail.com>
2014-01-23 08:22:40 -0500

    completed

--- crm.rb
+++ crm.rb
@@ -1,15 +1,9 @@
 require_relative 'contact'
 require_relative 'rolodex'
 
-# NEED TO DO RETRIEVE CONTACT >> MODIFY CONTACT METHODS.
-# FORMAT CONTACT LIST
 
 class CRA
 
-	# @rolodex.new_contact("Frank", "Gallager", "123-234-3456", "frank@alibiroom.com", "Do not invite for dinner anymore")
-	# @rolodex.new_conatct("Dexter", "Morgan", "555-666-7777", "dmorgan@miamipd.com", "Isn't returning messages")
-	# @rolodex.new_conatct("Tyrion", "Lannister", "333-222-1111", "tyrion@smallhand.com", "Send wedding gift")
-	
 	def initialize
 	end
 
@@ -17,7 +11,7 @@ class CRA
 # WORKING
 	def self.introduction
 		puts "\n\n<<< Welcome to your Customer Relationship Management Application. >>>\n\n"
-		puts "Would you like to see the main menu? (1)Y (2)N\n\n"
+		puts "Would you like to see the main menu? Enter number: (1)Y (2)N\n\n"
 		user_selected = gets.chomp.to_i
 		if user_selected == 1
 			main_menu
@@ -30,77 +24,53 @@ class CRA
 
 # WORKING	
 	def self.print_main_menu
-		puts " "
-		puts "What would you like to do?"
-		puts "[1] Add a new contact"
-		puts "[2] Modify an existing contact"
-		puts "[3] Delete a contact"
-	 	puts "[4] Display all the contacts"
-	 	puts "[5] Exit application"
-	 	puts "Choose option number: "
+			puts " "
+			puts "What would you like to do?"
+			puts "[1] Add a new contact"
+			puts "[2] Modify an existing contact"
+			puts "[3] Delete a contact"
+		 	puts "[4] Display all the contacts"
+		 	puts "[5] Exit application"
+		 	puts "Choose option number: "
 	end
 
 # WORKING
 	def self.main_menu
-        @rolodex=Rolodex.new
-        option=0
-        while (option!=6)
-		print_main_menu
-		user_selected = gets.to_i
-		call_option(user_selected)
-		end
+			@rolodex=Rolodex.new
+			option=0
+	    while (option!=5)
+			print_main_menu
+			user_selected = gets.to_i
+			call_option(user_selected)
+			end
 	end
 
 # WORKING
 	def self.call_option(user_selected)
 	  	add_new_contact if user_selected == 1
-	  	@rolodex.retrieve_contact if user_selected == 2
-		delete_contact if user_selected == 3
-	  	@rolodex.display_contacts if user_selected == 5
-	  	quit if user_selected == 6
+	  	@rolodex.modify_contact if user_selected ==2
+			@rolodex.delete_contact if user_selected == 3
+	  	@rolodex.display_contacts if user_selected == 4
+	  	quit if user_selected == 5
 	end
 # WORKING
 	def self.quit 
-		puts "\e[H\e[2J"
-		puts "\n<<< Goodbye and thank you for using this CRM app. >>>\n\n\n"
+			puts "\e[H\e[2J"
+			puts "\n<<< Goodbye and thank you for using CRM >>>\n\n\n"
 	end
 # WORKING
 	def self.add_new_contact
-		puts "Please enter contact information"
-		puts "First Name:"
-		@first_name = gets.chomp.capitalize
-		puts "Last Name:"
-		@last_name = gets.chomp.capitalize
-		puts "Email address:"
-		@email = gets.chomp.downcase
-		puts "Notes:"
-		@notes = gets.chomp
-		@rolodex.add_new_contact(@first_name, @last_name, @email, @notes)		
+			puts "Please enter contact information"
+			puts "First Name:"
+			@first_name = gets.chomp.capitalize
+			puts "Last Name:"
+			@last_name = gets.chomp.capitalize
+			puts "Email address:"
+			@email = gets.chomp.downcase
+			puts "Notes:"
+			@notes = gets.chomp
+			@rolodex.add_new_contact(@first_name, @last_name, @email, @notes)		
 	end
-
-	# def self.find_contact
-	# 	puts "Please enter the last name of the contact you wish to find."
-	# 	last_name = gets.chomp.capitalize
-	# 	@rolodex.find_contact(last_name)
-	# end
-
-	# def self.modify_contact
-	# 	puts "Please enter the last name of the contact you wish to modify:"
-	# 	last_name = gets.chomp.capitalize
-	# 	contact = ROLODEX.find_contact(last_name)
-	# 	puts "PLease enter the attribute you wish to modify: (phone, email, or notes)"
-	# 	selection = gets.chomp.downcase
-	# 	ROLODEX.modify_contact
-	# 	main_menu
-	# end
-
-	def self.delete_contact
-		puts "Please enter the id number of the contact you wish to delete:"
-		id = gets.chomp.to_i
-		@rolodex.delete_contact(id)
-	end
-
-
 end
 
 CRA.introduction
--- rolodex.rb
+++ rolodex.rb
@@ -13,36 +13,60 @@ class Rolodex
 		@contacts << Contact.new(@id, first_name, last_name, email, notes)
 		@contacts.last
 		puts "Contact has been added to database."
-		puts @contacts[0].first_name
 	end
 	
-	def find_contact(last_name)
-		@contacts.select{ |contact| contact.last_name == last_name }
+# WORKING
+	def modify_contact
+		puts "\e[H\e[2J"
+		display_contacts
+		puts " "
+		puts "Please enter the id number of the contact you wish to modify:"
+			id = gets.chomp
+			@contacts.each do |contact|
+				if contact.id == id.to_i
+					puts "Enter the new first name:"
+					first_name = gets.chomp.capitalize
+					contact.first_name = first_name
+					puts "Enter the new last name:"
+					last_name = gets.chomp.capitalize
+					contact.last_name = last_name
+					puts "Enter the new email address:"
+					email = gets.chomp.capitalize
+					contact.email = email
+					puts "Enter the new new note:"
+					notes = gets.chomp.capitalize
+					contact.notes = notes
+				end
+			end
 	end
 
-	# def modify_contact(contact, selection, new_attr)
-	# 	if selection == "email"
-	# 	puts "Enter new email address:"
-	# 	new_attr = gets.chomp.downcase
-	# 	contact.email = new_attr
-	# 	elsif selection == "notes"
-	# 	puts "Enter new note:"
-	# 	new_attr = gets.chomp
-	# 	contact.notes = new_attr
-	# 	else
-	# 	puts "That was not a valid selection"
-	# 	end
-	
-	# end
+  
 # WORKING
-	def delete_contact(id)
-		@contacts.delete_if{ |contact| contact.id == @id }
-		puts "Contact has been removed from database."
+	def delete_contact
+		display_contacts
+		puts "Please enter the id number of the contact you wish to delete:"
+		delete_id = gets.chomp.to_i
+		@contacts.each do |contact|
+      	if delete_id == contact.id
+      	@contacts.delete(contact)
+        end
+    end
+       	puts "\e[H\e[2J"
+       	puts "Contact has been removed from database."
 	end
 
 # WORKING
 	def display_contacts
-		@contacts.each{ |x| puts "ID: #{x.id} Name: #{x.first_name} #{x.last_name} Email: #{x.email} Notes: #{x.notes}"}
-    end
+		puts "\e[H\e[2J"
+		puts " "
+		puts "CONTACT LIST".center(85)
+		puts "-" * 80
+		
+#  JUSTIFICATION TO BE FIXED!!!!!
+		@contacts.each{ |x| puts "ID: #{x.id} Name: #{x.first_name.ljust(10)} #{x.last_name.ljust(15)} Email: #{x.email.ljust(20)} Notes: #{x.notes.ljust(35)}"}
+		puts " "
+		puts "-" * 80
+		puts " "
+  end
 
 end
